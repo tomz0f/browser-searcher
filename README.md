55860eac37cf8022248254498bbcaa16c27b8c74
Rawns665 <rawns0909@gmail.com>
2022-05-04 00:45:30 +0300

    files added!

---
+++ README.md
@@ -0,0 +1,6 @@
+# A SHORTCUT FOR SEARCHING FROM CLI
+## Use program with adding environment variables.<br>
+## SO YOU CAN USE IT EVERYWHERE! (IT SO GOOD THING I THINK!)
+* - [X] ADD ALL SEARCH ENGINES<br>
+* - [X] TEST IT
+* - [X] GOOD!!!
---
+++ search.py
@@ -0,0 +1,45 @@
+import sys
+import os
+
+x = ""
+browser_queries = {
+    "google": "google.com/search?q=",
+    "g": "google.com/search?q=",
+    "duckduckgo": "ducduckgo.com/?q=",
+    "ddg": "duckduckgo.com/?q=",
+    "yandex": "yandex.com/search/?text=",
+    "y": "yandex.com/search/?text=",
+    "yahoo": "https://search.yahoo.com/search?p="
+}
+browser_lists = [x for x in browser_queries.keys()]
+def getLink(browser_name = "google", search_query = ""):
+    search_query = search_query.replace(' ', '+')
+    full_link = f'{browser_queries[browser_name]}{search_query}'
+    os.system(f"start chrome {full_link}")
+
+def checkBrowser():
+    run = True
+    while(run):
+        print("Browser Choices:\n-----------------\n1) Google (google/g)\n2) DuckDuckGo (ddg/duckduckgo)\n3) Yandex (yandex/y)\n4) Yahoo (yahoo)", end = "\n")
+        browser_choice = input("Give a browser name for search: ")
+        if browser_choice in browser_lists:
+            run = False
+    return browser_choice
+try:
+    query = sys.argv[1]
+    if (query in browser_lists and (sys.argv[-1] not in browser_lists)):
+        browser_choice = query
+        search_query = sys.argv[-1]
+        getLink(browser_choice, search_query)
+
+    if (query not in browser_lists and (sys.argv[-1] != "" or sys.argv[-1] != None)):
+        search_query = sys.argv[-1]
+        browser_choice = checkBrowser()
+        getLink(browser_choice, search_query)
+    
+
+except IndexError:
+    browser_choice = checkBrowser()
+    search_query = input("Give a search query for search: ")
+    getLink(browser_choice, search_query)
+
